524ea153aa5b13b83408438505421d778d02a8f8
karthik Govindasamy <karthikg164@gmail.com>
2018-05-02 14:50:50 +0530

    recursion in python

--- recursion_in_python.py
+++ recursion_in_python.py
@@ -11,8 +11,19 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
+def fibonacci(n):
+    if n<2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
 
 #test code
 print 'factorial of 15 is {}'.format(factorial(15))
 print 'factorial of 5 is {}'.format(factorial(5))
 print 'factorial of 9 is {}'.format(factorial(9))
+print fibonacci(3)
+print fibonacci(8)
+print fibonacci(2)
+print fibonacci(15)
+print fibonacci(18)
